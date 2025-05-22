32e4c2e4d17dab801f9f18e95701737060dcd74b
Sinister71 <166879166+Sinister71@users.noreply.github.com>
2025-05-22 17:17:34 -0400

    Add files via upload

---
+++ Votação/com.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost'; 
+$dbname = 'Votacao'; 
+$username = 'usuario';
+$password = 'senha'; 
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+}
+?>
---
+++ Votação/index.php
@@ -0,0 +1,14 @@
+<? php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Votação dos Nigga</title>
+</head>
+<body>
+    <div class="w-full h-screen bg-gray-800"></div>
+</body>
+</html>
